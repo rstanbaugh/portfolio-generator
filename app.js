65215fc9de018eba0e073ce75b936a622afef0a4
rstanbaugh <rich_bootcamp@icloud.com>
2022-05-23 17:07:52 -0400

    added inquirer module

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// const [name, github] = profileDataArgs;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+// console.log(inquirer);
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
